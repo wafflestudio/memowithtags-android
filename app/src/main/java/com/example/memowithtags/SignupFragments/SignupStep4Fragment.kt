0085831380660c774751388ec6d2a98e34bcdfbc
huiwooj <ashleyhuiwoo@gmail.com>
2025-03-17 20:57:06 +0900

    signup on fragment

--- app/src/main/java/com/example/memowithtags/SignupFragments/SignupStep4Fragment.kt
+++ app/src/main/java/com/example/memowithtags/SignupFragments/SignupStep4Fragment.kt
@@ -7,27 +7,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.memowithtags.R
-import com.example.memowithtags.databinding.FragmentSignupStep2Binding
+import com.example.memowithtags.databinding.FragmentSignupStep4Binding
 
-class SignupStep2Fragment : Fragment() {
-    private var _binding: FragmentSignupStep2Binding? = null
+class SignupStep4Fragment : Fragment() {
+    private var _binding: FragmentSignupStep4Binding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSignupStep2Binding.inflate(inflater, container, false)
+        _binding = FragmentSignupStep4Binding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // 다음 단계로 이동
-        binding.nextButton.setOnClickListener {
-            findNavController().navigate(R.id.action_step2_to_step3)
-        }
     }
 
     override fun onDestroyView() {
